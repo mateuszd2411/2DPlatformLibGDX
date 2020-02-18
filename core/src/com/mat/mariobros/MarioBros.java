b503be9d5f10b6e0bcf299e186569a2c2a29624a
Mateusz <mateuszd2411@gmail.com>
2020-02-18 21:24:48 +0100

    4

--- core/src/com/mat/mariobros/MarioBros.java
+++ core/src/com/mat/mariobros/MarioBros.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mat.mariobros.Screens.PlayScreen;
 
 public class MarioBros extends Game {
+	public static final int V_WIDTH = 400;
+	public static final int V_HIGHT = 208;
+
 	public SpriteBatch batch;
 
 	
