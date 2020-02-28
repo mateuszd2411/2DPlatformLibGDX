c1875c491e209374408f3f13c4e6d7cfeba2e501
Mateusz <mateuszd2411@gmail.com>
2020-02-28 13:00:57 +0100

    15

--- core/src/com/mat/mariobros/MarioBros.java
+++ core/src/com/mat/mariobros/MarioBros.java
@@ -1,10 +1,14 @@
 package com.mat.mariobros;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mat.mariobros.Screens.PlayScreen;
 
 public class MarioBros extends Game {
+
 	public static final int V_WIDTH = 400;
 	public static final int V_HIGHT = 208;
 	public static final float PPM = 100;
@@ -17,17 +21,35 @@ public class MarioBros extends Game {
 
 	public SpriteBatch batch;
 
+	public static AssetManager manager;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		manager = new AssetManager();
+		manager.load("audio/music/mario_music.ogg", Music.class);
+		manager.load("audio/sounds/coin.wav", Sound.class);
+		manager.load("audio/sounds/bump.wav", Sound.class);
+		manager.load("audio/sounds/breakblock.wav", Sound.class);
+		manager.finishLoading();
+
+
 		setScreen(new PlayScreen(this));
 
 	}
 
 	@Override
+	public void dispose() {
+		super.dispose();
+		manager.dispose();
+		batch.dispose();
+	}
+
+	@Override
 	public void render () {
 		super.render();
+
 	}
 
 }
